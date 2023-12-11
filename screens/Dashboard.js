e087a70ffa4277c2a5074a370a50479a5a318858
Ummara-hussain <ummarahussain9@gmail.com>
2023-12-11 20:18:18 +0500

    done

--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { View, Text, Button, StyleSheet } from 'react-native'
+import { View, Text, Button, StyleSheet, Pressable } from 'react-native'
 import { collection, query, where, onSnapshot, db, updateStatus } from '../config/firebase'
 
 export default function Dashboard({ navigation }) {
@@ -26,21 +26,20 @@ export default function Dashboard({ navigation }) {
 
     return <View style={styles.container}>
         <Text style={styles.title}>Driver</Text>
-        
+
         {rides.map(item => {
             return <View style={styles.rides}>
-                <Text>Pickup Location:{item.pickup.name}</Text>
-                <Text>Destination Location:{item.destination.name}</Text>
-                <Text>Ride:{item.rideType}</Text>
-                <Text>Fare: Rs.{Math.round(item.fare)}</Text>
-                <Button title='Accept' onPress={() => {
+                <Text style={styles.text}>Pickup Location:{item.pickup.name}</Text>
+                <Text style={styles.text}>Destination Location:{item.destination.name}</Text>
+                <Text style={styles.text}>Ride:{item.rideType}</Text>
+                <Text style={styles.text}>Fare: Rs.{Math.round(item.fare)}</Text>
+                <Pressable style={styles.button} onPress={() => {
                     updateStatus(item.id, 'accepted')
                     navigation.navigate('Ride', item)
-                }} />
-                <Text style={{ textAlign: 'center' }}>OR</Text>
-                <Button title='Reject' onPress={() => {
+                }} ><Text style={styles.text}>Accept</Text></Pressable>
+                <Pressable style={styles.button} color='red' title='Reject' onPress={() => {
                     updateStatus(item.id, 'rejected')
-                }} />
+                }} ><Text style={styles.text}>Decline</Text></Pressable>
 
             </View>
         })}
@@ -51,18 +50,38 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 10,
-        justifyContent: 'center',
+        // justifyContent: 'center',
+        backgroundColor: 'black',
     },
     title: {
-        fontSize: 25,
+        fontSize: 30,
         padding: 2,
         textAlign: 'center',
-        margin: 1
+        margin: 1,
+        color: 'white',
+        fontWeight: 'bold'
+    },
+    text: {
+        color: 'white',
+        fontSize: 17
     },
     rides: {
-        border: '1px solid black',
+        borderStyle: 'dotted',
+        borderWidth: 2,
         margin: 3,
-        padding: 5,
-        height: '31%'
+        padding: 10,
+        height: '29%',
+        borderColor: 'white',
     },
+    button: {
+        borderColor: 'white',
+        // backgroundColor:'grey',
+        width: 100,
+        borderWidth: 1,
+        height: 30,
+        margin:4,
+        marginLeft:100,
+        paddingLeft: 19,
+    }
+
 })
